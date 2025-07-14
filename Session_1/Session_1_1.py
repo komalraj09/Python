44377b8d866f86e8e3649808d3f8407efeee7df9
komalraj09 <78505879+komalraj09@users.noreply.github.com>
2025-07-14 11:13:46 -0400

    Create Session_1_1.py

---
+++ Session_1/Session_1_1.py
@@ -0,0 +1,15 @@
+print("Hello, Python session")
+
+age = 10
+name = "Komal"
+isPresent = True
+
+ # To print Var 
+print (age)
+print(name)
+print(isPresent)
+
+# type Function find out data type
+
+print(type(name))
+
