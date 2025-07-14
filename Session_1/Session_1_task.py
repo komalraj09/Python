39e1a9ba35f449d96e4086d4d8bcbff6f4b359da
komalraj09 <78505879+komalraj09@users.noreply.github.com>
2025-07-14 17:33:54 -0400

    Add files via upload

---
+++ Session_1/Session_1_task.py
@@ -0,0 +1,17 @@
+#Ratan Naval Tata was an Indian industrialist and philanthropist. He served as the chairman of Tata Group and Tata Sons from 1991 to 2012 and he held the position of interim chairman from October 2016 to February 2017.
+
+name = input("Name : ")
+company = input ("company name : ")
+position = input("position : ")
+time_period = input("time : ")
+
+
+#name = "Ratan Naval Tata" 
+#company = "Tata Group and Tata Sons"
+#position = "chairman"
+#time_period = "October 2016 to February 2017"
+
+
+print(f"{name} was an Indian industrialist and philanthropist. He served as the chairman of {company} from 1991 to 2012 and he held the position of {position} from {time_period}")
+
+
