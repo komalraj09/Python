860a54e5622f5a3c98d98a69746a07a37b0eab4d
komalraj09 <78505879+komalraj09@users.noreply.github.com>
2025-07-14 11:14:51 -0400

    Add files via upload

---
+++ Session_1/session_1_2.py
@@ -0,0 +1,9 @@
+name = "komal"
+age = 20
+
+#format data
+print ("My name is", name)
+
+#formatted string
+print(f"My name is {name} & my age is {age}")
+print(f"multiplication of {3} is {3*3}")
---
+++ Session_1/session_1_3.py
@@ -0,0 +1,10 @@
+#user input, -- input function, always return as a string 
+
+name = input ("Enter your name: ")
+print(name)
+
+num = int(input("Enter your age:")) # type casting , change data type 
+print(type(num))
+print(num)
+
+
